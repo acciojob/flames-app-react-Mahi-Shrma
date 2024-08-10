f86234a47798f626723dd2ef424c792ab47f4070
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-08-10 19:40:43 +0000

    added code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import FlameApp from "./components/App";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<FlameApp/>, document.getElementById("root"));
